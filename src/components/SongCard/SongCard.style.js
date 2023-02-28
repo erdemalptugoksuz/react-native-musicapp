fc0b9abeff659864dcb88ca8d72548fffa014c04
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-02-28 19:28:41 +0300

    Fixed soldout albums.

--- src/components/SongCard/SongCard.style.js
+++ src/components/SongCard/SongCard.style.js
@@ -20,12 +20,27 @@ export default StyleSheet.create({
     fontWeight: 'bold',
   },
   info_container: {
+    flex: 1,
     flexDirection: 'row',
+    alignItems: 'center',
   },
   year: {
     fontSize: 12,
     marginLeft: 10,
     color: 'grey',
     fontWeight: 'bold',
+  },
+  soldout_container: {
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 5,
+    padding: 5,
+  },
+  soldout_title: {
+    color: 'red',
+    fontSize: 12,
+  },
+  content_container: {
+    flexDirection: 'row',
   },
 });
